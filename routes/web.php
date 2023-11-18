e00ac499a4cbbc7e2ef8987831cae1e9b25ffa5a
Darizzzz <darisfauzan47@gmail.com>
2023-11-18 12:45:12 +0700

    Uts laravel

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,38 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pertemuan4/home');
+    return view('UTS laravel/home');
 });
 
-Route::get('about', function () {
-    return view('about');
+Route::get('produk', function () {
+    return view('UTS lravel/produk');
 });
 
-Route::get('mahasiswa', function () {
-    return view('pertemuan4/mahasiswa');
+Route::get('array',function(){
+    for ($i=1; $i <= 5; $i++) { 
+        echo 'Hello World ' . $i . 'x<br>';
+    }
+});
+
+
+
+Route::get('form', function () {
+    return view('UTS laavel/form');
 });
 Route::get('profile', function () {
     return view('profile');
+});
+Route::get('produk', function () {
+    $kodeproduk = ['BRG001', 'BRG002'];
+    $namaproduk = ['Buku', 'Pena'];
+    $jenisproduk = ['Alat Tulis', 'Alat Tulis'];
+    $harga = [20000, 15000];
+    $jumlah = count($kodeproduk);
+    return view('UTS laravel/produk', compact('kodeproduk', 'namaproduk', 'jenisproduk', 'harga', 'jumlah'));
+});
+
+Route::get('/form', function () {
+    $jenisProduk = ['Jenis Produk', 'Alat Tulis', 'Elektronik', 'Sembako'];
+    $jumlahproduk = count($jenisProduk);
+    return view('UTS laravel/form', compact('jenisProduk', 'jumlahproduk'));
 });
